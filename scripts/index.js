1d59b17b360af0f507014802e4f2173557b766a6
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-09 18:20:29 +0200

    gets all data by region

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const CORSapi = "https://intense-mesa-62220.herokuapp.com/";
 
-document.getElementById("Asia").addEventListener("click", () => print("Asia"));
+document
+  .getElementById("Asia")
+  .addEventListener("click", () => showRegionData("Asia"));
 document
   .getElementById("Europe")
   .addEventListener("click", () => print("Europe"));
@@ -14,6 +16,29 @@ document
   .getElementById("Oceania")
   .addEventListener("click", () => print("Oceania"));
 
+const countriesList = {};
+
+const regionList = ["Asia", "Europe", "Africa", "Americas", "Oceania"];
+
+async function createCountriesListObject() {
+  // Runs on region list and creates an object that holds arrays of countries per region.
+  for (region of regionList) {
+    const countriesArr = [];
+    const countries = await axios.get(
+      CORSapi + "https://restcountries.herokuapp.com/api/v1/region/" + region
+    );
+
+    for (let i = 0; i < countries.data.length; i++) {
+      countriesArr.push(countries.data[i].cca2);
+    }
+    countriesList[region] = countriesArr;
+  }
+}
+
+createCountriesListObject();
+
+// TODO create country dropdown func
+
 async function getData(countryCode) {
   //gets country code, returns deathRate, confirmed, critical, deaths, recovered
   const data = await axios.get(
@@ -22,30 +47,12 @@ async function getData(countryCode) {
   return data.data.data.latest_data;
 }
 
-async function getCountriesByRegion(region) {
-  //gets list of countries by region and saves them for later use
-  const countries = await axios.get(
-    CORSapi + "https://restcountries.herokuapp.com/api/v1/region/" + region
-  );
-  const countriesArr = [];
-  for (let i = 0; i < countries.data.length; i++) {
-    countriesArr.push(countries.data[i].cca2);
+async function showRegionData(region) {
+  console.log(`countriesList[region] is ${countriesList[region]}`);
+  for (let i = 0; i < countriesList[region].length; i++) {
+    //TODO store this data in an object for all region's countries?
+    // or each country with its own data?
+    // TODO store in local storage unless it's already there
+    console.log(await getData(countriesList[region][i]));
   }
-  return countriesArr;
-}
-
-const countriesList = {
-  Asia: getCountriesByRegion("Asia"),
-  Europe: getCountriesByRegion("Europe"),
-  Africa: getCountriesByRegion("Africa"),
-  Americas: getCountriesByRegion("Americas"),
-  Oceania: getCountriesByRegion("Oceania"),
-};
-
-async function print(region) {
-  console.log(await countriesList[region]);
-}
-
-async function showData() {
-  getData("AF");
 }
