982207008ca9deab7bf1bd234862a34f29efae55
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-09 17:06:58 +0200

    preloading country list

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,51 @@
 const CORSapi = "https://intense-mesa-62220.herokuapp.com/";
 
-document
-  .getElementById("Asia")
-  .addEventListener("click", () => getCountriesByRegion("Asia"));
+document.getElementById("Asia").addEventListener("click", () => print("Asia"));
 document
   .getElementById("Europe")
-  .addEventListener("click", () => getCountriesByRegion("Europe"));
+  .addEventListener("click", () => print("Europe"));
 document
   .getElementById("Africa")
-  .addEventListener("click", () => getCountriesByRegion("Africa"));
+  .addEventListener("click", () => print("Africa"));
 document
   .getElementById("Americas")
-  .addEventListener("click", () => getCountriesByRegion("Americas"));
+  .addEventListener("click", () => print("Americas"));
 document
   .getElementById("Oceania")
-  .addEventListener("click", () => getCountriesByRegion("Oceania"));
+  .addEventListener("click", () => print("Oceania"));
 
 async function getData(countryCode) {
+  //gets country code, returns deathRate, confirmed, critical, deaths, recovered
   const data = await axios.get(
     CORSapi + "http://corona-api.com/countries/" + countryCode
   );
-  console.log(data.data.data.latest_data); //gets deathRate, confirmed, critical, deaths, recovered
+  return data.data.data.latest_data;
 }
 
 async function getCountriesByRegion(region) {
+  //gets list of countries by region and saves them for later use
   const countries = await axios.get(
     CORSapi + "https://restcountries.herokuapp.com/api/v1/region/" + region
   );
-  console.log(countries);
+  const countriesArr = [];
   for (let i = 0; i < countries.data.length; i++) {
-    console.log(countries.data[i].cca2);
+    countriesArr.push(countries.data[i].cca2);
   }
+  return countriesArr;
 }
 
-// getData("AF");
-// Asia Europe Africa Americas Oceania
-// getCountriesByRegion("Oceania");
+const countriesList = {
+  Asia: getCountriesByRegion("Asia"),
+  Europe: getCountriesByRegion("Europe"),
+  Africa: getCountriesByRegion("Africa"),
+  Americas: getCountriesByRegion("Americas"),
+  Oceania: getCountriesByRegion("Oceania"),
+};
+
+async function print(region) {
+  console.log(await countriesList[region]);
+}
+
+async function showData() {
+  getData("AF");
+}
